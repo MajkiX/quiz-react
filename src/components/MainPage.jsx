93760eb4efbc08579437c3b7d79379d54b8341d3
MajkiX <Mateuszzawadzki01@interia.pl>
2024-10-15 12:20:34 +0200

    Media querry for mobile

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -11,10 +11,11 @@ const MainPage = () => {
     return (
         <div className={style()}>
             <h1>Welcome to Quiz</h1>
-            <h2>this is a small project created to practice working with React</h2>
+            <h2>This is a small project created to practice working with React</h2>
             <h2>If you want to play, select the difficulty level</h2>
             <h2>
-                the game consists of 10 questions, after answering them your result will be displayed</h2>
+                The game consists of 10 questions, after answering them your result will be displayed
+            </h2>
         </div>
     );
 }
