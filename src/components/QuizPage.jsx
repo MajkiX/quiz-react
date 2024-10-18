a88a7e6e7273668b79d5548d2cbc79144b64eecf
MajkiX <Mateuszzawadzki01@interia.pl>
2024-10-18 15:05:20 +0200

    Next build fixes

--- src/components/QuizPage.jsx
+++ src/components/QuizPage.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import axios from 'axios';
 import anime from 'animejs'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
